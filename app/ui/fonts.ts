893aa2d18128bff61be684d04debc96674a86979
Liliana Vasquez <lady.vasquez@globant.com>
2024-04-23 14:33:20 -0500

    First commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import { Inter, Lusitana } from 'next/font/google'; // Primary and secondary fonts
+ 
+export const inter = Inter({ subsets: ['latin'] }); // Subset I want to load
+export const lusitana = Lusitana({ weight: ['400', '700'], subsets: ['latin'] });
